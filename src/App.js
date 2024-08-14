00fb1fc2ad9f15bf347c0486ad06eab87d0f150a
GabiDourado <gabi.dourados2xd@gmail.com>
2024-08-14 15:27:43 -0300

    acertos

--- src/App.js
+++ src/App.js
@@ -2,12 +2,6 @@ import logo from './logo.svg';
 import './css/App.css';
 import Home from './pages/Home';
 import Menu from './components/Menu';
-import Nike from './pages/nike';
-import Adidas from './pages/adidas';
-import Fila from './pages/fila';
-import Puma from './pages/puma';
-import Champions from './pages/champions';
-import Vans from './pages/vans';
 
 function App() {
   return (
